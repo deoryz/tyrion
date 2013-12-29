67ea492f5e039e6d7ae6003b5d285258d1063d31
[deory <[deoryzpandu@gmail.com]>
2013-12-29 15:00:49 +0700

    add model user and user_group

---
+++ protected/models/UserGroup.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * This is the model class for table "user_group".
+ *
+ * The followings are the available columns in table 'user_group':
+ * @property integer $id
+ * @property string $name
+ * @property string $label
+ * @property string $date_update
+ */
+class UserGroup extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return UserGroup the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'user_group';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('name, label, date_update', 'required'),
+			array('name, label', 'length', 'max'=>100),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, name, label, date_update', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'name' => 'Name',
+			'label' => 'Label',
+			'date_update' => 'Date Update',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('label',$this->label,true);
+		$criteria->compare('date_update',$this->date_update,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
