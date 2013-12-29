859ff70efd180231a54f1ea663c68d39197e8187
[deory <[deoryzpandu@gmail.com]>
2013-12-29 15:01:20 +0700

    add migrate and controller site

---
+++ protected/modules/admin/controllers/MigrateController.php
@@ -0,0 +1,21 @@
+<?php
+
+class MigrateController extends Controller
+{
+	public function actionIndex()
+	{
+	    $commandPath = Yii::app()->getBasePath() . DIRECTORY_SEPARATOR . 'commands';
+	    $runner = new CConsoleCommandRunner();
+	    $runner->addCommands($commandPath);
+	    $commandPath = Yii::getFrameworkPath() . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'commands';
+	    $runner->addCommands($commandPath);
+	    $args = array('yiic', 'migrate', '--interactive=0');
+	    ob_start();
+	    $runner->run($args);
+	    $content =  htmlentities(ob_get_clean(), null, Yii::app()->charset);
+	    echo "<pre>";
+	    echo $content;
+	    echo "</pre>";
+	}
+
+}
